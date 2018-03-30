ed005c7e108c7a71e7ae0e239cc8a7c80ba6d751
Stephanie Wong <swongwrites@gmail.com>
2018-03-30 16:34:37 -0700

    Complete mergeSortedArrays

---
+++ MergeSortedArray/mergeSortedArray.js
@@ -0,0 +1,40 @@
+// *****************************************************
+// ***************** MERGE SORTED ARRAYS ***************
+// *****************************************************
+
+// How would you merge two sorted arrays?
+
+function mergeSortedArrays(a, b){
+  var merged = [], 
+      aElm = a[0],
+      bElm = b[0],
+      i = 1,
+      j = 1;
+  
+  if(a.length ==0)
+    return b;
+  if(b.length ==0)
+    return a;
+  /* 
+  if aElm or bElm exists we will insert to merged array
+  (will go inside while loop)
+   to insert: aElm exists and bElm doesn't exists
+             or both exists and aElm < bElm
+    this is the critical part of the example            
+  */
+  while(aElm || bElm){
+   if((aElm && !bElm) || aElm < bElm){
+     merged.push(aElm);
+     aElm = a[i++];
+   }   
+   else {
+     merged.push(bElm);
+     bElm = b[j++];
+   }
+  }
+  return merged;
+}
+
+// >>>>>>>>>>>>>>>>>> TEST CASES >>>>>>>>>>>>>>>>>> //
+
+console.log(mergeSortedArrays([2,5,6,9], [1,2,3,29])); // [1, 2, 2, 3, 5, 6, 9, 29]
